eb4dfdc0b1a746d79e086d067dc65a2d74a47adf
Sergio <smc0709@hotmail.com>
2024-11-21 18:23:33 +0100

    Museo v2

--- Assets/TutorialInfo/Scripts/Readme.cs
+++ Assets/TutorialInfo/Scripts/Readme.cs
@@ -1,16 +1,3 @@
-﻿using System;
-using UnityEngine;
-
-public class Readme : ScriptableObject
-{
-    public Texture2D icon;
-    public string title;
-    public Section[] sections;
-    public bool loadedLayout;
-
-    [Serializable]
-    public class Section
-    {
-        public string heading, text, linkText, url;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c09c93844f93eb252a6fb39c222d169c63a63e96008fd040b9e7628c3e7ccc2e
+size 302
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Museum
-
-This repository contains the Unity Course 'Museum' project.
+version https://git-lfs.github.com/spec/v1
+oid sha256:606bf658a979196594f05aa739c29f6dd64a3190cd21e2794c47634c2ba92727
+size 70
